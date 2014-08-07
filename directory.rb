ddc98418ff54ca51c79d6011a08e8b7a55418908
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-07 21:40:30 +0100

    Making the directory interactive

--- directory.rb
+++ directory.rb
@@ -1,18 +1,21 @@
-# let's put all students into an array
-students =[
-[name: "Peter Smith", :cohort => :november],
-[name: "Jane Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november],
-[name: "Marianne Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november],
-[name: "Peter Smith", :cohort => :november]
-]
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << {:name => name, :cohort => :november}
+		puts "Now we have #{students.length} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+	# return array of students
+	students
+end
 # and then print them
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -31,6 +34,7 @@ def print_footer(names)
 end
 
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
