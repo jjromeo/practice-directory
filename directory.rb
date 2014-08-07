02bf6ced212cc5f5d420efbcf04976d0a26a1e4c
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-07 21:59:42 +0100

    Version 6 exercise 4

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ end
 
 def print(details)
 	details.each_with_index do |x, y|
-		puts "#{y + 1}. #{x[:name]} (#{x[:cohort]} cohort)" if x[:name].downcase.start_with?('a')
+		puts "#{y + 1}. #{x[:name]} (#{x[:cohort]} cohort)" if x[:name].length < 12
 	end
 end
 
