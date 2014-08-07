a110594bdfbd63652ea23579a75874c753b6a212
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-07 20:16:40 +0100

    adding comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,7 @@
+#first we print the list of students
+puts "The students of my cohort at Makers Academy"
+puts "-----------------"
+
 puts "Peter Smith"
 puts "Peter Smith"
 puts "Peter Smith"
@@ -23,6 +27,8 @@ puts "Peter Smith"
 puts "Peter Smith"
 puts "Peter Smith"
 
+#finally, we print the total
 print "Overall we have "
+#it's important that print() doesn't add newline characters
 print 24
 puts " great students"
