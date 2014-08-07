a6efe659e5f57a493ba35c51f845bb6296790087
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-07 20:35:11 +0100

    making students an array and iterating to puts the student names

--- directory.rb
+++ directory.rb
@@ -1,34 +1,39 @@
 #first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "-----------------"
+# let's put all students into an array
+students =[
+"Peter Smith",
+"Jane Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Marianne Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith",
+"Peter Smith"
+]
+# and then print them
 
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
-puts "Peter Smith"
+students.each do |x|
+	puts x
+end
 
 #finally, we print the total
-print "Overall we have "
+puts "Overall, we have #{students.length} great students "
 #it's important that print() doesn't add newline characters
-print 24
-puts " great students"
